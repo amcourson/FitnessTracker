7515902720ae399933f09dbe43f3469cc065de03
amcourson <amcourson09@sbcglobal.net>
2021-05-11 15:24:19 -0500

    updating mongodb

--- Develop/server.js
+++ Develop/server.js
@@ -23,11 +23,3 @@ app.listen(PORT, () => {
 
 
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://<fitnesstrackeruser>:<fitnesstrackeruser>@cluster0.dorua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
